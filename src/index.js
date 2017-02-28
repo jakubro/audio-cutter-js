1a9fdd55c2793f9f2758d491551ecb50957c2216
Jakub Roman <jakub@jakubroman.sk>
2017-03-01 00:24:27 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 $(document).ready(function () {
   var config = _songcut_config;
 
-  var $form = createForm(config.target.uri, config.target.method, config.source.metadata);
-  $('body').append($form);
-
-  setupWaveSurfer('#waveform');
+  var $form = createForm('body', config.target.uri, config.target.method, config.source.metadata);
+  var waveSurfer = createWaveSurfer('#waveform', config.source.uri);
 
   $('#btn-play').on('click', function () {
-    wavesurfer.playPause();
+    waveSurfer.playPause();
   });
   $('#btn-save').on('click', function () {
     $form.submit();
   });
 });
 
-function createForm(action, method, inputArr) {
+function createForm(rootElement, action, method, inputArr) {
   var $form = $('<form></form>');
   $form.attr('action', action);
   $form.attr('method', method);
-  for(var name in inputArr) {
-    if(inputArr.hasOwnProperty(name)) {
-      var $input = $('<input type="text">');
+  for (var name in inputArr) {
+    if (inputArr.hasOwnProperty(name)) {
+      var $input = $('<input type="hidden">');
       $input.attr('name', name);
       $input.attr('value', inputArr[name]);
       $form.append($input);
     }
   }
+  $(rootElement).append($form);
   return $form;
 }
 
-function setupWaveSurfer(rootElement) {
-  var wavesurfer = WaveSurfer.create({
+function createWaveSurfer(rootElement, songUri) {
+  var waveSurfer = WaveSurfer.create({
     container: rootElement,
     splitChannels: true
   });
-
-  wavesurfer.load(config.source.songuri);
-  wavesurfer.on('ready', function () {
+  waveSurfer.load(songUri);
+  waveSurfer.on('ready', function () {
   });
+  return waveSurfer;
 }
