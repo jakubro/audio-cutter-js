747ab354c4b35c0e9e00b3784e1b5693afc642b0
Jakub Roman <jakub@jakubroman.sk>
2017-03-01 22:37:49 +0100

    next

--- src/index.js
+++ src/index.js
@@ -1,40 +1,71 @@
 $(document).ready(function () {
   var config = _songcut_config;
 
-  var $form = createForm('body', config.target.uri, config.target.method, config.source.metadata);
-  var waveSurfer = createWaveSurfer('#waveform', config.source.uri);
-
-  $('#btn-play').on('click', function () {
-    waveSurfer.playPause();
-  });
-  $('#btn-save').on('click', function () {
-    $form.submit();
-  });
-});
-
-function createForm(rootElement, action, method, inputArr) {
   var $form = $('<form></form>');
-  $form.attr('action', action);
-  $form.attr('method', method);
-  for (var name in inputArr) {
-    if (inputArr.hasOwnProperty(name)) {
+  $form.attr('action', config.target.uri);
+  $form.attr('method', config.target.method);
+  for (var name in config.source.metadata) {
+    if (config.source.metadata.hasOwnProperty(name)) {
       var $input = $('<input type="hidden">');
       $input.attr('name', name);
-      $input.attr('value', inputArr[name]);
+      $input.attr('value', config.source.metadata[name]);
       $form.append($input);
     }
   }
-  $(rootElement).append($form);
-  return $form;
-}
+  $('body').append($form);
 
-function createWaveSurfer(rootElement, songUri) {
-  var waveSurfer = WaveSurfer.create({
-    container: rootElement,
-    splitChannels: true
+  var wavesurfer = WaveSurfer.create({
+    container: config.player.element,
+    splitChannels: true,
+    interact: false,
+    scrollParent: false,
+    cursorColor: '#c63ab2',
+    progressColor: '#4BB9F7',
+    waveColor: '#7e7e7e'
   });
-  waveSurfer.load(songUri);
-  waveSurfer.on('ready', function () {
+
+  wavesurfer.load(config.source.uri);
+  wavesurfer.on('ready', function () {
+
+    var region = wavesurfer.addRegion({
+      start: 0,
+      end: Math.min(30, wavesurfer.getDuration()),
+      loop: true,
+      drag: true,
+      resize: true,
+      color: 'hsla(330, 100%, 20%, 0.2)'
+    });
+
+    region.on('update-end', function () {
+      // update length of region to allowed max
+      region.update({
+        end: Math.min(region.start + 30, region.end)
+      });
+      // reset position to start of region
+      if(wavesurfer.isPlaying() && region.end < wavesurfer.getCurrentTime()) {
+        wavesurfer.stop();
+      }
+      wavesurfer.seekTo(region.start / wavesurfer.getDuration());
+    });
+
+    $('#btn-play').on('click', function () {
+      wavesurfer.play();
+    });
+
+    $('#btn-pause').on('click', function () {
+      wavesurfer.pause();
+    });
+
+    $('#btn-stop').on('click', function () {
+      // reset position to start of region
+      wavesurfer.stop();
+      wavesurfer.seekTo(region.start / wavesurfer.getDuration());
+    });
+
+    $('#btn-save').on('click', function () {
+      $form.find('input[name="startTime"]').val(region.start);
+      $form.find('input[name="endTime"]').val(region.end);
+      // $form.submit();
+    });
   });
-  return waveSurfer;
-}
+});
